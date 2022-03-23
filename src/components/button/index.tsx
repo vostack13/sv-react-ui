f9a06016cbda1ecd131236469673d75d51124e65
vostack13 <stanislav.vozikov@gmail.com>
2022-03-23 14:03:19 +0300

    Update

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
+import cn from "classnames";
+import './styles.css'
 
 interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, React.AriaAttributes  {}
 interface Props extends ButtonProps {}
 
-const Button: FC<Props> = ({ children, ...props }) => <button {...props}>{children}</button>
+const Button: FC<Props> = ({ children, ...props }) => <button className={cn('SV-Button')} {...props}>{children}</button>
 
 export default Button;
