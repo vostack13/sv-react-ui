c226e3c4db65e66e5cff3d28321cf6c6a9894ac9
vostack13 <stanislav.vozikov@gmail.com>
2022-03-23 12:26:18 +0300

    Update

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   
-  const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-  const plugins = [ "macros" ];
+  const presets = [ "@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript" ];
+  const plugins = [ "macros", "@babel/plugin-transform-typescript" ];
 
   return {
     presets,
